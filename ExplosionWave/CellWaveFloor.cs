d08d9b58ff3655101cf5c76afc754053c94ab6fb
Romulo San <romulossan@gmail.com>
2018-12-01 21:11:27 -0200

    ok

--- ExplosionWave/CellWaveFloor.cs
+++ ExplosionWave/CellWaveFloor.cs
@@ -6,31 +6,44 @@ public class CellWaveFloor : ObjectWave {
 
 	public Renderer rend;
 	public LocalOffsetValue offset;
-    public CustomPunch punch;
+    public CustomPunchBase punch;
 
     private bool blockAmplitude;
 
 	public override void IniWave(float Time,float amplitude,Vector3 pos, float maxAmplitude){
         if (amplitude < 0 || blockAmplitude){
-            offset.amplitude = 0;
-            punch.amplitude = 0;
+            if (offset)
+                offset.amplitude = 0;
+            if (punch)
+                punch.amplitude = 0;
         }else{
-            offset.amplitude += amplitude;
-            offset.amplitude = Mathf.Clamp(offset.amplitude, 0, maxAmplitude);
-            punch.amplitude += amplitude;
-            punch.amplitude = Mathf.Clamp( punch.amplitude, 0, maxAmplitude);
+            if (offset)
+            {
+                offset.amplitude += amplitude;
+                offset.amplitude = Mathf.Clamp(offset.amplitude, 0, maxAmplitude);
+            }
+
+            if (punch)
+            {
+                punch.amplitude += amplitude;
+                punch.amplitude = Mathf.Clamp(punch.amplitude, 0, maxAmplitude);
+            }
+
             base.IniWave (Time, amplitude,pos,maxAmplitude);
         }
 	}
 
     public override void PlayAnim(){
-        punch.Punch();
+        if (punch)
+            punch.Punch();
         base.PlayAnim();
     }
 
     public void ResetAmplitude(){
-        offset.SetAmplitude(0);
-        punch.amplitude = 0;
+        if (offset)
+            offset.SetAmplitude(0);
+        if (punch)
+            punch.amplitude = 0;
         blockAmplitude = false;
     }
 }
---
+++ ExplosionWave/ExplosionWave2D.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class ExplosionWave2D : MonoBehaviour {
+
+	public AnimationCurve curveAmplitude;
+	public AnimationCurve curveDistance;
+	public float timeLenght=1,amplitude=1;
+	public LayerMask cellfloor;
+	public float radius;
+
+	void Start(){
+		Destroy(gameObject, timeLenght * 3);
+		Collider2D[] cols = Physics2D.OverlapCircleAll(transform.position, radius, cellfloor);
+		foreach (var item in cols) {
+			ObjectWave c = item.GetComponent<ObjectWave> ();
+			if (c) {
+				float dis = Vector3.Distance (transform.position, c.transform.position);
+				dis = Mathf.InverseLerp (0, radius, dis);
+				c.IniWave (curveDistance.Evaluate(dis)*timeLenght, curveAmplitude.Evaluate(dis)* amplitude,transform.position,amplitude);
+			}
+		}
+	}
+
+	private void OnDrawGizmos() {
+		Gizmos.color = Color.yellow;
+		Gizmos.matrix = Matrix4x4.TRS(transform.position,Quaternion.identity,new Vector3(1,1,0));
+		Gizmos.DrawWireSphere(Vector3.zero,radius);
+	}
+}
